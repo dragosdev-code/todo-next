62668b5bd98ffebfde11e58acb78870ab138de69
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-01-25 18:13:11 +0100

    made the navigation

--- app/docs/[[...id]]/page.tsx
+++ app/docs/[[...id]]/page.tsx
@@ -1,6 +1,6 @@
 const DocsIdPage = ({ params, searchParams }) => {
   console.log({ params, searchParams })
-  return <div>docs id page {params.id.join(' / ')}</div>
+  return <div>docs id page {params.id?.join(' / ')}</div>
 }
 
 export default DocsIdPage
