252031724818b79e24aa18e253a44424391471c3
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-01-26 14:52:16 +0100

    loading state and error

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,6 +2,7 @@ import TodoList from '@/components/TodoList'
 import db from '@/utils/db'
 
 const getData = async () => {
+  await new Promise((resolve, reject) => setTimeout(() => reject(0), 2000))
   const todos = await db.todo.findMany({})
   console.log(todos)
   return todos
