232654fe1a0a6f782a38d357e3ee48c509d03491
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-01-26 15:25:31 +0100

    complete todo functionality

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,7 +2,6 @@ import TodoList from '@/components/TodoList'
 import db from '@/utils/db'
 
 const getData = async () => {
-  await new Promise((resolve, reject) => setTimeout(() => reject(0), 2000))
   const todos = await db.todo.findMany({})
   console.log(todos)
   return todos
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,5 +1,21 @@
+'use client'
+
+import { useTransition } from 'react'
+import { completeTodo } from '@/utils/actions'
+
 const Todo = ({ todo }) => {
-  return <div>{todo.content}</div>
+  const [isPending, startTransition] = useTransition()
+
+  return (
+    <div
+      className={`border border-black/20 cursor-pointer ${
+        todo.completed ? 'line-through text-gray-400' : ''
+      }`}
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+    >
+      {todo.content}
+    </div>
+  )
 }
 
 export default Todo
