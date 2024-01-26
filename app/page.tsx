d134ccba1d091c6adcb182afcc8aa88ca9ece7cb
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-01-26 13:40:47 +0100

    example of fetching data in server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div></div>
+const getData = async () => {
+  await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000))
+  return { data: [1, 2, 3] }
+}
+
+export default async function Home() {
+  const data = await getData()
+  console.log({ data })
+  return (
+    <div>
+      {data.data.map((el) => (
+        <p key={el}>{el}</p>
+      ))}
+    </div>
+  )
 }
